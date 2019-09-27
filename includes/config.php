81874e9984bb904cd447bb868f08696ef6e00a4a
Fazrin <nirzaf@gmail.com>
2019-09-27 10:21:39 +0530

    get book and get student files removed

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,17 @@
-<?php 
+<?php
 // DB credentials.
-define('DB_HOST','sltjranking.db.14038887.75d.hostedresource.net');
-define('DB_USER','sltjranking');
-define('DB_PASS','Sltjfazrin@2019');
-define('DB_NAME','sltjranking');
+//define('DB_HOST','sltjranking.db.14038887.75d.hostedresource.net');
+//define('DB_USER','sltjranking');
+//define('DB_PASS','Sltjfazrin@2019');
+//define('DB_NAME','sltjranking');
 // Establish database connection.
+
+//local host
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','sltjranking');
+
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
